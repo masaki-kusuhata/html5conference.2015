e94c1e917e30099fe9e203c6b429a5fb56d4caf7
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-03-24 16:38:58 +0900

    クロスドメイン

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
+'use strict';
+
 console.log('HTML5 Conference in Kagoshima');
+
+// http://jws.jalan.net/APILite/HotelSearch/V1/?key=sco14c4a95fb8c&pref=460000&l_area=460200&s_area=460202
+var url = 'http://jws.jalan.net/APILite/HotelSearch/V1/?key=sco14c4a95fb8c&pref=460000&l_area=460200&s_area=460202';
